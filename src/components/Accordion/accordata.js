da7e458031da04aead69fdbae76a82379c59fea2
Ajaynirm <ajay@AJ-MacBook-Air-M1.local>
2024-07-17 08:07:00 +0530

    first commit with startingcomponents

---
+++ src/components/Accordion/accordata.js
@@ -0,0 +1,22 @@
+export const data =[
+    {
+        id: '1',
+        question : 'what are accordion componenets',
+        answer : 'Accordion components are user interface elements used for organisisng and presenting'
+    },
+    {
+        id: '2',
+        question : 'what are accordion componenets',
+        answer : 'Accordion components are user interface elements used for organisisng and presenting'
+    },
+    {
+        id: '3',
+        question : 'what are accordion componenets',
+        answer : 'Accordion components are user interface elements used for organisisng and presenting'
+    },
+    {
+        id: '4',
+        question : 'what are accordion componenets',
+        answer : 'Accordion components are user interface elements used for organisisng and presenting'
+    }
+]
