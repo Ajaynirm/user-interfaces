6d34fe9859921f7ef94ad7fc3897c1e8afd3619a
Ajaynirm <ajay@AJ-MacBook-Air-M1.local>
2024-07-17 11:19:55 +0530

    second commit with aacordion component updated

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import "./accordion.css"
+import React, { useState } from "react";
+import "./accordion.css";
+import { data } from "./accordata.js";
 
 const Accordion = () => {
+  const [selected, setSelected] = useState(null);
+  const handleSingleClick = (id) => {
+    console.log(id);
+    setSelected(selected == id ? null : id); 
+    console.log(selected);
+    
+  };
   return (
-    <div>Accordion</div>
-  )
-}
+    <>
+    <div className="wrapper">
+      <div className="accordion">
+        <h1 className="head">Accordion</h1>
+        {data && data.length > 0 ? (
+          data.map((item, index) => (
+            <div className="item" key={index}>    
+              <div onClick={() => handleSingleClick(item.id)} className="title">
+                <h3>{item.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === item.id ? (
+                <div className="answer">{item.answer}</div>
+              ) : (
+                <div></div>
+              )}
+            </div>
+          ))
+        ) : (
+          <div>No data found</div>
+        )}
+      </div>
+    </div>
+    </>
+  );
+};
 
-export default Accordion
+export default Accordion;
