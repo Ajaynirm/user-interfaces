ddd4576faf3ad53ab1f18c48983efb43fd6c76f5
Ajaynirm <ajay@AJ-MacBook-Air-M1.local>
2024-07-17 17:37:43 +0530

    multiple selection toggle selection functionality implemented with responsive design and content for data updated

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -4,30 +4,43 @@ import { data } from "./accordata.js";
 
 const Accordion = () => {
   const [selected, setSelected] = useState(null);
+  const [enableMulti,setEnableMulti] = useState(false);
+  const [multiSelected,setMultiSelected]=useState([]);
   const handleSingleClick = (id) => {
     console.log(id);
     setSelected(selected == id ? null : id); 
     console.log(selected);
+    setMultiSelected([]);
     
   };
-  const ChangeFunction = () => {
+  const handleMultiClick = (id) => {
+    let copy =[...multiSelected];
+    const findOccur =copy.indexOf(id);
+    if(findOccur === -1){
+      copy.push(id);
+    }else{
+      copy.splice(findOccur,1);
+    }
+    setMultiSelected(copy);
+  } 
+  console.log(multiSelected,enableMulti);
+  
 
-  }
   return (
     <>
-    <div className="wrapper">
+    <div className="wrapper ">
     <div className="head">Accordion</div>
-    <button className="btn__change" onClick={"ChangeFunction"}>Multiple select</button>
+    <button className="btn__change" onClick={() => setEnableMulti(!enableMulti)}>Toggle Multi-select</button>
       <div className="accordion">
 
         {data && data.length > 0 ? (
           data.map((item, index) => (
-            <div className="item" key={index}>    
-              <div onClick={() => handleSingleClick(item.id)} className="title">
+            <div className="item " key={index}>    
+              <div onClick={() => (enableMulti ? handleMultiClick(item.id) : handleSingleClick(item.id))} className="title">
                 <h3>{item.question}</h3>
                 <span>+</span>
               </div>
-              {selected === item.id ? (
+              {(selected === item.id && !enableMulti)  || (multiSelected.includes(item.id) && enableMulti) ? (
                 <div className="answer">{item.answer}</div>
               ) : (
                 <div></div>
--- src/components/Accordion/accordata.js
+++ src/components/Accordion/accordata.js
@@ -1,22 +1,22 @@
 export const data =[
     {
         id: '1',
-        question : 'what are accordion componenets',
-        answer : 'Accordion components are user interface elements used for organisisng and presenting'
+        question : 'what is ReactJS',
+        answer : 'React is a popular JavaScript library for building user interfaces, particularly for single-page applications. It was developed by Facebook and is now maintained by Facebook and a community of individual developers and companies.'
     },
     {
         id: '2',
-        question : 'what are accordion componenets',
-        answer : 'Accordion components are user interface elements used for organisisng and presenting'
+        question : 'what is the latest version of Javascript',
+        answer : 'The latest version of JavaScript, also known as ECMAScript 2023 (ES14), was finalized in June 2023. This update introduces several new features aimed at improving developer productivity and code efficiency'
     },
     {
         id: '3',
-        question : 'what are accordion componenets',
-        answer : 'Accordion components are user interface elements used for organisisng and presenting'
+        question : 'what is mongoDB',
+        answer : 'MongoDB is a NoSQL database that uses a document-oriented data model. It is designed to store, retrieve, and manage large volumes of unstructured or semi-structured data. Here are some key features and concepts related to MongoDB'
     },
     {
         id: '4',
-        question : 'what are accordion componenets',
-        answer : 'Accordion components are user interface elements used for organisisng and presenting'
+        question : 'what is nodeJS',
+        answer : 'Node.js is an open-source, cross-platform runtime environment that allows developers to execute JavaScript code outside of a web browser'
     }
 ]
